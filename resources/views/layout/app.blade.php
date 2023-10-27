af8d8f62bf28ff39e7d1b00dc9cf7b28008affdc
lorenzo-neri <lorenzo21neri@gmail.com>
2023-10-27 15:08:48 +0200

    add layout structure with test  from controller

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>@yield('page-title', 'Laravel Model Controller')</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+
+</head>
+
+<body>
+
+    <!-- header -->
+    @include('layout.partials.header')
+
+    <main>
+        @yield('content')
+    </main>
+
+    <!-- footer -->
+    @include('layout.partials.footer')
+
+</body>
+
+</html>
